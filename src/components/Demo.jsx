a557b54efb8884c57328a365c5521651361c570d
NuzNuz <trnazeer14@gmail.com>
2023-05-17 21:11:22 -0400

    Change website theme to dark mode

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -80,7 +80,7 @@ const Demo = () => {
                   <img src={copied === item.url ? tick: copy} alt='copy_icon' className='w-[40%] h-[40%] object-contain'/>
                   
                 </div>
-                <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                <p className='flex-1 font-satoshi text-blue-500 font-medium text-sm truncate'>
                   {item.url}
                 </p>
               
@@ -99,11 +99,11 @@ const Demo = () => {
             ) : (
               article.summary && (
                 <div className='flex flex-col gap-3'>
-                  <h2 className='font-satoshi font-bold text-greay-600 text-xl'>
+                  <h2 className='font-satoshi font-bold text-white text-xl'>
                     Article <span className='blue_gradient'>Summary</span>
                   </h2>
                   <div className='summary_box'>
-                    <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
+                    <p className='font-inter font-medium text-sm text-white'>{article.summary}</p>
                   </div>
 
                 </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,18 +6,19 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center align-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+        
+        <span className='text-white text-2xl'>Swift<span className='blue_gradient'>Sum</span></span>
         <button
           type='button'
           onClick={() => window.open('https://github.com/NuzNuz')}
-          className='black_btn'>
+          className='github_button'>
           GitHub
         </button>
       </nav>
 
       <h1 className='head_text '>
         Summarize Articles with <br className='max-md:hidden'/>
-        <span className='orange_gradient'>
+        <span className='blue_gradient'>
           OpenAI GPT-4
         </span>
       </h1>
